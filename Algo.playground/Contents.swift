2d6a3daed553f9788ccb1d7fe61c9ca6e8a691b9
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2020-10-08 16:53:44 +0200

    Add Phantom types sample with swift

--- Algo.playground/Contents.swift
+++ Algo.playground/Contents.swift
@@ -378,6 +378,36 @@ extension String {
         return attributedString.string
     }
 }
+// A Fantastic Experiment with Phantom Types
+enum Waiting { }
+enum CoinInserted { }
+enum Fetching { }
+enum Serving { }
+
+struct Transition<From, To> {
+    
+}
+
+struct Machine<State> {
+    func transition<To>(with transition: Transition<State, To>) -> Machine<To> {
+        .init()
+        
+    }
+}
+
+let start = Transition<Waiting, CoinInserted>()
+let selectionMode = Transition<CoinInserted, Fetching>()
+let delivery = Transition<Fetching, Serving>()
+let reset = Transition<Serving, Waiting>()
+
+let m1 = Machine<Waiting>()
+
+let m2 = m1.transition(with: start)
+let m3 = m2.transition(with: selectionMode)
+let m4 = m3.transition(with: delivery)
+let m5 = m4.transition(with: reset)
+
+let m6 = m5.transition(with: start)
 
 
 // Handle User TextView
